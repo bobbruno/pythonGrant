7337f513a5fa103f156c9176a2045da3a949a4cd
Roberto Bruno Martins <bobbruno@gmail.com>
2014-09-03 17:16:46 +0200

    Finally got rid of NaNs

--- mamba.py
+++ mamba.py
@@ -69,7 +69,197 @@ def munge_data(df_orig):
     # Sum the # of published papers
     
     numPapers = df.groupby('Grant.Application.ID')['A..1', 'A.1','B.1', 'C.1','Number.of.Successful.Grant.1','Number.of.Unsuccessful.Grant.1'].sum()
-    numPapers = pd.DataFrame(numPapers) 
+
+    
+    df['Contract.Value.Band...see.note.A'].fillna('A', inplace=True)
+    df['Contract.Value.Band...see.note.A']=df['Contract.Value.Band...see.note.A'].apply(lambda x: ord(x.rstrip(' ')))
+    
+    # converting categories to dummy variables
+    
+    grant_cats = pd.get_dummies(df['Grant.Category.Code'], dummy_na=True)    
+    grant_cats['Grant.Application.ID']=df['Grant.Application.ID']
+    grant_cats = pd.groupby(grant_cats, 'Grant.Application.ID')[grant_cats.columns].min()  
+    grant_cats = pd.DataFrame(grant_cats)
+    
+    # imputing missing percentages for RFCD.Percentage columns with the mean
+    df['RFCD.Percentage.1'].fillna(df['RFCD.Percentage.1'].mean(), inplace=True)
+    df['RFCD.Percentage.2'].fillna(df['RFCD.Percentage.2'].mean(), inplace=True)
+    df['RFCD.Percentage.3'].fillna(df['RFCD.Percentage.3'].mean(), inplace=True)
+    df['RFCD.Percentage.4'].fillna(df['RFCD.Percentage.4'].mean(), inplace=True)
+    df['RFCD.Percentage.5'].fillna(df['RFCD.Percentage.5'].mean(), inplace=True)
+    
+    # doing the same as above with SEO.Percentage columns
+    df['SEO.Percentage.1'].fillna(df['SEO.Percentage.1'].mean(), inplace=True)
+    df['SEO.Percentage.2'].fillna(df['SEO.Percentage.2'].mean(), inplace=True)
+    df['SEO.Percentage.3'].fillna(df['SEO.Percentage.3'].mean(), inplace=True)
+    df['SEO.Percentage.4'].fillna(df['SEO.Percentage.4'].mean(), inplace=True)
+    df['SEO.Percentage.5'].fillna(df['SEO.Percentage.5'].mean(), inplace=True)
+    rfcds = combine_columns(df, 'RFCD.Code.', 'RFCD.Percentage.')
+    seos = combine_columns(df, 'SEO.Code.', 'SEO.Percentage.')
+    
+    # Get rid of everything we don't need
+    # REMINDER - LATER COME BACK AND DEAL WITH DEPARTMENT, FACULTY, NO YEARS AT FACULTY, PHD, ETC
+    df.drop(['A..1', u'A.1', u'B.1', u'C.1', u'Country.of.Birth.1', u'Dept.No..1', u'Faculty.No..1',
+           u'Home.Language.1', u'No..of.Years.in.Uni.at.Time.of.Grant.1', u'Number.of.Successful.Grant.1',
+           u'Number.of.Unsuccessful.Grant.1', u'Role.1', u'Sponsor.Code', u'With.PHD.1', u'Year.of.Birth.1',
+           u'SEO.Code.4', u'SEO.Code.5', u'SEO.Code.1', u'SEO.Code.2', u'SEO.Code.3', u'RFCD.Code.1',
+           u'RFCD.Code.2', u'RFCD.Code.3', u'RFCD.Code.4', u'RFCD.Code.5', 'Grant.Category.Code', u'RFCD.Percentage.1', u'RFCD.Percentage.2', u'RFCD.Percentage.3', u'RFCD.Percentage.4', u'RFCD.Percentage.5', u'SEO.Percentage.1', u'SEO.Percentage.2', u'SEO.Percentage.3', u'SEO.Perdef munge_data(df_orig):
+    df = df_orig.copy()
+    del df['Person.ID.1']
+    # Find the oldest investigator's birth date
+    oldest = pd.DataFrame(df.groupby('Grant.Application.ID')['Year.of.Birth.1'].min())
+    
+    
+    # Get the number of investigators for each role
+    numRole = pd.get_dummies(df['Role.1'])
+    numRole['Grant.Application.ID'] = df['Grant.Application.ID']
+    numRole = pd.groupby(numRole, 'Grant.Application.ID')[numRole.columns].sum()
+    
+    # Get the % of aussies
+    numAussies = pd.get_dummies(df['Country.of.Birth.1'])
+    numAussies['Grant.Application.ID'] = df['Grant.Application.ID']
+    numAussies = pd.groupby(numAussies, 'Grant.Application.ID')[numAussies.columns].sum()
+    
+    # We just imputed all values with NaN (no country info) to zero
+    prcAussies = pd.DataFrame((numAussies['Australia'] / numAussies.sum(axis = 1)).fillna(0), columns = ['% Australians'])
+    
+    # Sum the # of published papers
+    
+    numPapers = df.groupby('Grant.Application.ID')['A..1', 'A.1','B.1', 'C.1','Number.of.Successful.Grant.1','Number.of.Unsuccessful.Grant.1'].sum()
+
+    
+    df['Contract.Value.Band...see.note.A'].fillna('A', inplace=True)
+    df['Contract.Value.Band...see.note.A']=df['Contract.Value.Band...see.note.A'].apply(lambda x: ord(x.rstrip(' ')))
+    
+    # converting categories to dummy variables
+    
+    grant_cats = pd.get_dummies(df['Grant.Category.Code'], dummy_na=True)    
+    grant_cats['Grant.Application.ID']=df['Grant.Application.ID']
+    grant_cats = pd.groupby(grant_cats, 'Grant.Application.ID')[grant_cats.columns].min()  
+    grant_cats = pd.DataFrame(grant_cats)
+    
+    # imputing missing percentages for RFCD.Percentage columns with the mean
+    df['RFCD.Percentage.1'].fillna(df['RFCD.Percentage.1'].mean(), inplace=True)
+    df['RFCD.Percentage.2'].fillna(df['RFCD.Percentage.2'].mean(), inplace=True)
+    df['RFCD.Percentage.3'].fillna(df['RFCD.Percentage.3'].mean(), inplace=True)
+    df['RFCD.Percentage.4'].fillna(df['RFCD.Percentage.4'].mean(), inplace=True)
+    df['RFCD.Percentage.5'].fillna(df['RFCD.Percentage.5'].mean(), inplace=True)
+    
+    # doing the same as above with SEO.Percentage columns
+    df['SEO.Percentage.1'].fillna(df['SEO.Percentage.1'].mean(), inplace=True)
+    df['SEO.Percentage.2'].fillna(df['SEO.Percentage.2'].mean(), inplace=True)
+    df['SEO.Percentage.3'].fillna(df['SEO.Percentage.3'].mean(), inplace=True)
+    df['SEO.Percentage.4'].fillna(df['SEO.Percentage.4'].mean(), inplace=True)
+    df['SEO.Percentage.5'].fillna(df['SEO.Percentage.5'].mean(), inplace=True)
+    rfcds = combine_columns(df, 'RFCD.Code.', 'RFCD.Percentage.')
+    seos = combine_columns(df, 'SEO.Code.', 'SEO.Percentage.')
+    
+    # Get rid of everything we don't need
+    # REMINDER - LATER COME BACK AND DEAL WITH DEPARTMENT, FACULTY, NO YEARS AT FACULTY, PHD, ETC
+    df.drop(['A..1', u'A.1', u'B.1', u'C.1', u'Country.of.Birth.1', u'Dept.No..1', u'Faculty.No..1',
+           u'Home.Language.1', u'No..of.Years.in.Uni.at.Time.of.Grant.1', u'Number.of.Successful.Grant.1',
+           u'Number.of.Unsuccessful.Grant.1', u'Role.1', u'Sponsor.Code', u'With.PHD.1', u'Year.of.Birth.1',
+           u'SEO.Code.4', u'SEO.Code.5', u'SEO.Code.1', u'SEO.Code.2', u'SEO.Code.3', u'RFCD.Code.1',
+           u'RFCD.Code.2', u'RFCD.Code.3', u'RFCD.Code.4', u'RFCD.Code.5', 'Grant.Category.Code', u'RFCD.Percentage.1', u'RFCD.Percentage.2', u'RFCD.Percentage.3', u'RFCD.Percentage.4', u'RFCD.Percentage.5', u'SEO.Percentage.1', u'SEO.Percentage.2', u'SEO.Percentage.3', u'SEO.Percentage.4', u'SEO.Percentage.5',], inplace = True, axis = 1)
+    df.drop_duplicates(inplace = True)
+    df.set_index('Grant.Application.ID', inplace=True)
+    
+    finalDf = pd.merge(df, oldest, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, numRole, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, prcAussies, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, numPapers, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, grant_cats, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, rfcds, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, seos, left_index = True, rightdef munge_data(df_orig):
+    df = df_orig.copy()
+    del df['Person.ID.1']
+    # Find the oldest investigator's birth date
+    oldest = pd.DataFrame(df.groupby('Grant.Application.ID')['Year.of.Birth.1'].min())
+    
+    
+    # Get the number of investigators for each role
+    numRole = pd.get_dummies(df['Role.1'])
+    numRole['Grant.Application.ID'] = df['Grant.Application.ID']
+    numRole = pd.groupby(numRole, 'Grant.Application.ID')[numRole.columns].sum()
+    
+    # Get the % of aussies
+    numAussies = pd.get_dummies(df['Country.of.Birth.1'])
+    numAussies['Grant.Application.ID'] = df['Grant.Application.ID']
+    numAussies = pd.groupby(numAussies, 'Grant.Application.ID')[numAussies.columns].sum()
+    
+    # We just imputed all values with NaN (no country info) to zero
+    prcAussies = pd.DataFrame((numAussies['Australia'] / numAussies.sum(axis = 1)).fillna(0), columns = ['% Australians'])
+    
+    # Sum the # of published papers
+    
+    numPapers = df.groupby('Grant.Application.ID')['A..1', 'A.1','B.1', 'C.1','Number.of.Successful.Grant.1','Number.of.Unsuccessful.Grant.1'].sum()
+
+    
+    df['Contract.Value.Band...see.note.A'].fillna('A', inplace=True)
+    df['Contract.Value.Band...see.note.A']=df['Contract.Value.Band...see.note.A'].apply(lambda x: ord(x.rstrip(' ')))
+    
+    # converting categories to dummy variables
+    
+    grant_cats = pd.get_dummies(df['Grant.Category.Code'], dummy_na=True)    
+    grant_cats['Grant.Application.ID']=df['Grant.Application.ID']
+    grant_cats = pd.groupby(grant_cats, 'Grant.Application.ID')[grant_cats.columns].min()  
+    grant_cats = pd.DataFrame(grant_cats)
+    
+    # imputing missing percentages for RFCD.Percentage columns with the mean
+    df['RFCD.Percentage.1'].fillna(df['RFCD.Percentage.1'].mean(), inplace=True)
+    df['RFCD.Percentage.2'].fillna(df['RFCD.Percentage.2'].mean(), inplace=True)
+    df['RFCD.Percentage.3'].fillna(df['RFCD.Percentage.3'].mean(), inplace=True)
+    df['RFCD.Percentage.4'].fillna(df['RFCD.Percentage.4'].mean(), inplace=True)
+    df['RFCD.Percentage.5'].fillna(df['RFCD.Percentage.5'].mean(), inplace=True)
+    
+    # doing the same as above with SEO.Percentage columns
+    df['SEO.Percentage.1'].fillna(df['SEO.Percentage.1'].mean(), inplace=True)
+    df['SEO.Percentage.2'].fillna(df['SEO.Percentage.2'].mean(), inplace=True)
+    df['SEO.Percentage.3'].fillna(df['SEO.Percentage.3'].mean(), inplace=True)
+    df['SEO.Percentage.4'].fillna(df['SEO.Percentage.4'].mean(), inplace=True)
+    df['SEO.Percentage.5'].fillna(df['SEO.Percentage.5'].mean(), inplace=True)
+    rfcds = combine_columns(df, 'RFCD.Code.', 'RFCD.Percentage.')
+    seos = combine_columns(df, 'SEO.Code.', 'SEO.Percentage.')
+    
+    # Get rid of everything we don't need
+    # REMINDER - LATER COME BACK AND DEAL WITH DEPARTMENT, FACULTY, NO YEARS AT FACULTY, PHD, ETC
+    df.drop(['A..1', u'A.1', u'B.1', u'C.1', u'Country.of.Birth.1', u'Dept.No..1', u'Faculty.No..1',
+           u'Home.Language.1', u'No..of.Years.in.Uni.at.Time.of.Grant.1', u'Number.of.Successful.Grant.1',
+           u'Number.of.Unsuccessful.Grant.1', u'Role.1', u'Sponsor.Code', u'With.PHD.1', u'Year.of.Birth.1',
+           u'SEO.Code.4', u'SEO.Code.5', u'SEO.Code.1', u'SEO.Code.2', u'SEO.Code.3', u'RFCD.Code.1',
+           u'RFCD.Code.2', u'RFCD.Code.3', u'RFCD.Code.4', u'RFCD.Code.5', 'Grant.Category.Code', u'RFCD.Percentage.1', u'RFCD.Percentage.2', u'RFCD.Percentage.3', u'RFCD.Percentage.4', u'RFCD.Percentage.5', u'SEO.Percentage.1', u'SEO.Percentage.2', u'SEO.Percentage.3', u'SEO.Percentage.4', u'SEO.Percentage.5',], inplace = True, axis = 1)
+    df.drop_duplicates(inplace = True)
+    df.set_index('Grant.Application.ID', inplace=True)
+    
+    finalDf = pd.merge(df, oldest, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, numRole, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, prcAussies, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, numPapers, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, grant_cats, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, rfcds, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, seos, left_index = True, rightdef munge_data(df_orig):
+    df = df_orig.copy()
+    del df['Person.ID.1']
+    # Find the oldest investigator's birth date
+    oldest = pd.DataFrame(df.groupby('Grant.Application.ID')['Year.of.Birth.1'].min())
+    
+    
+    # Get the number of investigators for each role
+    numRole = pd.get_dummies(df['Role.1'])
+    numRole['Grant.Application.ID'] = df['Grant.Application.ID']
+    numRole = pd.groupby(numRole, 'Grant.Application.ID')[numRole.columns].sum()
+    
+    # Get the % of aussies
+    numAussies = pd.get_dummies(df['Country.of.Birth.1'])
+    numAussies['Grant.Application.ID'] = df['Grant.Application.ID']
+    numAussies = pd.groupby(numAussies, 'Grant.Application.ID')[numAussies.columns].sum()
+    
+    # We just imputed all values with NaN (no country info) to zero
+    prcAussies = pd.DataFrame((numAussies['Australia'] / numAussies.sum(axis = 1)).fillna(0), columns = ['% Australians'])
+    
+    # Sum the # of published papers
+    
+    numPapers = df.groupby('Grant.Application.ID')['A..1', 'A.1','B.1', 'C.1','Number.of.Successful.Grant.1','Number.of.Unsuccessful.Grant.1'].sum()
 
     
     df['Contract.Value.Band...see.note.A'].fillna('A', inplace=True)
@@ -117,17 +307,132 @@ def munge_data(df_orig):
     finalDf = pd.merge(finalDf, seos, left_index = True, right_index = True)
     
     #imputing ages with median
-    finalDf['Year.of.Birth.1'].fillna(finalDf['Year.of.Birth.1'].median(), inplace = True)
+    finalDf['Year.of.Birth.1'] = finalDf['Year.of.Birth.1'].fillna(finalDf['Year.of.Birth.1'].median())
     
     #imputing missing papers with 0
-    finalDf['A..1'].fillna(0, inplace = True)
-    finalDf['A.1'].fillna(0, inplace = True)
-    finalDf['B.1'].fillna(0, inplace = True)
-    finalDf['C.1'].fillna(0, inplace = True)
+    finalDf['A..1']=finalDf['A..1'].fillna(0)
+    finalDf['A.1']=finalDf['A.1'].fillna(0)
+    finalDf['B.1']=finalDf['B.1'].fillna(0)
+    finalDf['C.1']=finalDf['C.1'].fillna(0)
     
     #imputing missing successful and unsuccessful grants with 0
-    finalDf['Number.of.Successful.Grant.1'].fillna(0, inplace = True)
-    finalDf['Number.of.Unsuccessful.Grant.1'].fillna(0, inplace = True)
+    finalDf['Number.of.Successful.Grant.1']=finalDf['Number.of.Successful.Grant.1'].fillna(0)
+    finalDf['Number.of.Unsuccessful.Grant.1']=finalDf['Number.of.Unsuccessful.Grant.1'].fillna(0)
+        
+    del finalDf['Grant.Application.ID_y']
+    del finalDf['Grant.Application.ID_x']
+    finalDf['Proc.Start.Date'] = finalDf['Start.date'].apply(lambda x:
+                          time.mktime(datetime.datetime.strptime(x,'%d/%m/%y').timetuple()))
+   #splitting dataframe
+    mask = time_mask(finalDf)
+    finalDf_test = finalDf[mask]
+    finalDf_train = finalDf[-mask]
+    
+    #creating X, y splits for test and train dataframes
+    y_train = finalDf_train['Grant.Status'].values
+    del finalDf_train['Grant.Status']
+    del finalDf_train['Start.date']
+    X_train = finalDf_train.values
+    
+    y_test = finalDf_test['Grant.Status'].values
+    del finalDf_test['Grant.Status']
+    del finalDf_test['Start.date']
+    X_test = finalDf_test.values
+    
+    return X_train, y_train, X_test, y_test, finalDf_test, finalDf_train_index = True)
+    
+    #imputing ages with median
+    finalDf['Year.of.Birth.1'] = finalDf['Year.of.Birth.1'].fillna(finalDf['Year.of.Birth.1'].median()))
+    
+    #imputing missing papers with 0
+    finalDf['A..1']=finalDf['A..1'].fillna(0)
+    finalDf['A.1']=finalDf['A.1'].fillna(0)
+    finalDf['B.1']=finalDf['B.1'].fillna(0)
+    finalDf['C.1']=finalDf['C.1'].fillna(0)
+    
+    #imputing missing successful and unsuccessful grants with 0
+    finalDf['Number.of.Successful.Grant.1']=finalDf['Number.of.Successful.Grant.1'].fillna(0)
+    finalDf['Number.of.Unsuccessful.Grant.1']=finalDf['Number.of.Unsuccessful.Grant.1'].fillna(0)
+        
+    del finalDf['Grant.Application.ID_y']
+    del finalDf['Grant.Application.ID_x']
+    finalDf['Proc.Start.Date'] = finalDf['Start.date'].apply(lambda x:
+                          time.mktime(datetime.datetime.strptime(x,'%d/%m/%y').timetuple()))
+   #splitting dataframe
+    mask = time_mask(finalDf)
+    finalDf_test = finalDf[mask]
+    finalDf_train = finalDf[-mask]
+    
+    #creating X, y splits for test and train dataframes
+    y_train = finalDf_train['Grant.Status'].values
+    del finalDf_train['Grant.Status']
+    del finalDf_train['Start.date']
+    X_train = finalDf_train.values
+    
+    y_test = finalDf_test['Grant.Status'].values
+    del finalDf_test['Grant.Status']
+    del finalDf_test['Start.date']
+    X_test = finalDf_test.values
+    
+    return X_train, y_train, X_test, y_test, finalDf_test, finalDf_train_index = True)
+    
+    #imputing ages with median
+    finalDf['Year.of.Birth.1'] = finalDf['Year.of.Birth.1'].fillna(finalDf['Year.of.Birth.1'].median()))
+    
+    #imputing missing papers with 0
+    finalDf['A..1']=finalDf['A..1'].fillna(0)
+    finalDf['A.1']=finalDf['A.1'].fillna(0)
+    finalDf['B.1']=finalDf['B.1'].fillna(0)
+    finalDf['C.1']=finalDf['C.1'].fillna(0)
+    
+    #imputing missing successful and unsuccessful grants with 0
+    finalDf['Number.of.Successful.Grant.1']=finalDf['Number.of.Successful.Grant.1'].fillna(0)
+    finalDf['Number.of.Unsuccessful.Grant.1']=finalDf['Number.of.Unsuccessful.Grant.1'].fillna(0)
+        
+    del finalDf['Grant.Application.ID_y']
+    del finalDf['Grant.Application.ID_x']
+    finalDf['Proc.Start.Date'] = finalDf['Start.date'].apply(lambda x:
+                          time.mktime(datetime.datetime.strptime(x,'%d/%m/%y').timetuple()))
+   #splitting dataframe
+    mask = time_mask(finalDf)
+    finalDf_test = finalDf[mask]
+    finalDf_train = finalDf[-mask]
+    
+    #creating X, y splits for test and train dataframes
+    y_train = finalDf_train['Grant.Status'].values
+    del finalDf_train['Grant.Status']
+    del finalDf_train['Start.date']
+    X_train = finalDf_train.values
+    
+    y_test = finalDf_test['Grant.Status'].values
+    del finalDf_test['Grant.Status']
+    del finalDf_test['Start.date']
+    X_test = finalDf_test.values
+    
+    return X_train, y_train, X_test, y_test, finalDf_test, finalDf_traincentage.4', u'SEO.Percentage.5',], inplace = True, axis = 1)
+    df.drop_duplicates(inplace = True)
+    df.set_index('Grant.Application.ID', inplace=True)
+    
+    finalDf = pd.merge(df, oldest, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, numRole, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, prcAussies, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, numPapers, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, grant_cats, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, rfcds, left_index = True, right_index = True)
+    finalDf = pd.merge(finalDf, seos, left_index = True, right_index = True)
+    
+    #imputing ages with median
+    finalDf['Year.of.Birth.1'] = finalDf['Year.of.Birth.1'].fillna(finalDf['Year.of.Birth.1'].median())
+    
+    #imputing missing papers with 0
+    finalDf['A..1']=finalDf['A..1'].fillna(0)
+    finalDf['A.1']=finalDf['A.1'].fillna(0)
+    finalDf['B.1']=finalDf['B.1'].fillna(0)
+    finalDf['C.1']=finalDf['C.1'].fillna(0)
+    
+    #imputing missing successful and unsuccessful grants with 0
+    finalDf['Number.of.Successful.Grant.1']=finalDf['Number.of.Successful.Grant.1'].fillna(0)
+    finalDf['Number.of.Unsuccessful.Grant.1']=finalDf['Number.of.Unsuccessful.Grant.1'].fillna(0)
         
     del finalDf['Grant.Application.ID_y']
     del finalDf['Grant.Application.ID_x']
